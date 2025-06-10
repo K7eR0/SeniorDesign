bcce9eb2f6ef73ab2494bc7b6fc1688b9e12e9e6
K7eR0 <rcn045@gmail.com>
2025-06-10 12:59:23 -0400

    Arduino Code
    Added arduino code as a text file

--- OpenCVMain/ArduinoServerTest.py
+++ OpenCVMain/ArduinoServerTest.py
@@ -2,16 +2,17 @@ import serial
 import socket
 import time
 
-arduino = serial.Serial(port='COM12',   baudrate=115200, timeout=.1)
+arduino = serial.Serial(port='COM12',   baudrate=115200, timeout=0.2)
 
 def write_read(x):
     arduino.write(bytes(x, 'utf-8'))
-    time.sleep(0.05)
+    time.sleep(0.01)
     data = arduino.readline()
     return data
 
 while True:
-    num = input("Input a GPIO pin and state \n examples: (01 u) or (02 d)")
+    num = input("Input number of steps: ")
     value = write_read(num)
+    print("Previous Position: ")
     print(value)
     
