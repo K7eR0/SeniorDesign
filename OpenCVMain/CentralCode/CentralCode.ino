90f4740ee0cc67b85b2bfd11d68a200d6f1f14d6
K7eR0 <rcn045@gmail.com>
2025-07-07 20:22:52 -0400

    BLE Code working
    The BLE code is working however it is using a function that isn't ours. Will change later

--- OpenCVMain/CentralCode/CentralCode.ino
+++ OpenCVMain/CentralCode/CentralCode.ino
@@ -3,21 +3,178 @@
 
 void setup() {
   Serial.begin(115200);
-  Serial.setTimeout(1.5);
+  while (!Serial)
+    ;
 
-  if(!BLE.begin()){
-    Serial.println("Failed to initialize BLE"); //Shows if BLE module failed to activate
-    while(1);  
+  // begin initialization
+  if (!BLE.begin()) {
+    Serial.println("starting Bluetooth® Low Energy module failed!");
+
+    while (1)
+      ;
   }
+
+  Serial.println("Bluetooth® Low Energy Central scan");
+
+  // start scanning for peripheral
   BLE.scan();
 }
 
 void loop() {
-  BLEDevice peripheral = BLE.available(); // Check for available peripgerals
-  if (peripheral.localName() == "IMUWearable"){
+  // check if a peripheral has been discovered
+  BLEDevice peripheral = BLE.available();
+
+  if (peripheral) {
+    // discovered a peripheral
+    Serial.println("Discovered a peripheral");
+    Serial.println("-----------------------");
+
+    // print address
+    Serial.print("Address: ");
+    Serial.println(peripheral.address());
+
+    // print the local name, if present
+    if (peripheral.hasLocalName()) {
+      Serial.print("Local Name: ");
+      Serial.println(peripheral.localName());
+    }
+
+    // print the advertised service UUIDs, if present
+    if (peripheral.hasAdvertisedServiceUuid()) {
+      Serial.print("Service UUIDs: ");
+      for (int i = 0; i < peripheral.advertisedServiceUuidCount(); i++) {
+        Serial.print(peripheral.advertisedServiceUuid(i));
+        Serial.print(" ");
+      }
+      Serial.println();
+    }
+
+    // print the RSSI
+    Serial.print("RSSI: ");
+    Serial.println(peripheral.rssi());
+
+    Serial.println();
+
+    if (peripheral.localName() != "IMUWearable") {
+      return;
+    }
     BLE.stopScan();
-  }else{
-    BLE.scan();
+    monitorBLEperipheral(peripheral);
+    BLE.scanForUuid("180f");
   }
+}
+
+void monitorBLEperipheral(BLEDevice peripheral) { // THIS FUNCTION IS NOT OUR OWN AND IS PURELY FOR TESTING PURPOSES AND WILL BE REVERSE ENGINEERED AND REPLACED
+  // connect to the peripheral
+  Serial.println("Connecting ...");
+  if (peripheral.connect()) {
+    Serial.println("Connected");
+  } else {
+    Serial.println("Failed to connect!");
+    return;
+  }
+
+  // discover peripheral attributes
+  Serial.println("Discovering service 0xffe0 ...");
+  if (peripheral.discoverService("180F")) {
+    Serial.println("Service discovered");
+  } else {
+    Serial.println("Attribute discovery failed.");
+    peripheral.disconnect();
+
+    while (1);
+    return;
+  }
+
+  // retrieve the IMUSensorData characteristic
+  BLECharacteristic IMUSensorData = peripheral.characteristic("2A19");
+//  BLEStringCharacteristic IMUSensorData = peripheral.characteristic("2A19", BLERead | BLEWrite, 512);
   
+  // subscribe to the simple key characteristic
+  Serial.println("Subscribing to IMUSensorData characteristic ...");
+  if (!IMUSensorData) {
+    Serial.println("no IMUSensorData characteristic found!");
+    peripheral.disconnect();
+    return;
+  } 
+    else if (!IMUSensorData.canSubscribe()) {
+    Serial.println("IMUSensorData characteristic is not subscribable!");
+    peripheral.disconnect();
+    return;
+  } 
+    else if (!IMUSensorData.subscribe()) {
+    Serial.println("subscription failed!");
+    peripheral.disconnect();
+    return;
+  } 
+    else {
+    Serial.println("Subscribed to IMUSensorData characteristic");
+//    Serial.println("Press the right and left buttons on your SensorTag.");
+  }
+
+  while (peripheral.connected()) {
+    // while the peripheral is connected
+
+    // check if the value of the simple key characteristic has been updated
+    if (IMUSensorData.valueUpdated()) {
+//    if (IMUSensorData.written()){
+      // yes, get the value, characteristic is 1 byte so use byte value
+//      long value = 0 ;
+//      int ReceivingArray[2] = {0,0}; 
+
+// THIS SECTION CONVERTS THE RECEIVED CHARACTERISTIC FROM UNSIGNED CHAR TO STRING // 
+      String str;
+      int length = IMUSensorData.valueLength();
+      const uint8_t* val = IMUSensorData.value();
+      str.reserve(length);
+
+      for (int i = 0; i<length; i++){
+        str += (char)val[i];
+      }
+
+      Serial.println(str);
+        
+//      IMUSensorData.readValue(value);
+
+      
+//      Serial.println(value);
+//      IMUSensorData.readValue(ReceivingArray,2);
+//      Serial.println(ReceivingArray[0]);
+//      Serial.println(ReceivingArray[1]);
+//      for(int i = 0; i < 1; i++)
+//      {
+//        Serial.println(ReceivingArray[i]);
+//      }
+
+//      if (value & 0x01) {
+//        // first bit corresponds to the right button
+//        Serial.println("Right button pressed");
+//      }
+//
+//      if (value & 0x02) {
+//        // second bit corresponds to the left button
+//        Serial.println("Left button pressed");
+      }
+    }
+  
+
+  Serial.println("BLE disconnected!");
+}
+
+void readData(BLEDevice peripheral) { // THIS IS OUR FUNCTION BUT CURRENTLY DOESN'T WORK, IT WILL BE MODIFIED LATER
+  Serial.println("Found");
+  while (peripheral.connected()) {
+    BLECharacteristic IMUSensorData = peripheral.characteristic("2A19");
+    String receivedString = "";
+    IMUSensorData.read();
+    if (IMUSensorData.read()) {
+    String receivedString = "";
+    for (int i = 0; i < IMUSensorData.valueLength(); i++) {
+      receivedString += (char)IMUSensorData.value()[i];
+    }
+    Serial.print("Received String: ");
+    Serial.println(receivedString);
+
+  }
+}
 }
--- OpenCVMain/WearableCode/WearableCode/WearableCode.ino
+++ OpenCVMain/WearableCode/WearableCode/WearableCode.ino
@@ -2,29 +2,32 @@
 #include <ArduinoBLE.h>
 #include <Arduino.h>
 
-BLEService IMUService("180F"); //Set the service to IMU and entered UUID
-BLEStringCharacteristic IMUSensorData("2A19", BLERead | BLENotify, 512); //Set the data characteristics
+BLEService IMUService("180F");                                            //Set the service to IMU and entered UUID
+BLEStringCharacteristic IMUSensorData("2A19", BLERead | BLENotify, 512);  //Set the data characteristics
 void setup() {
 
   Serial.begin(9600);
-  while (!Serial);
+  while (!Serial)
+    ;
 
   if (!IMU.begin()) {
-    Serial.println("Failed to initialize IMU!"); //Shows if IMU module failed to activate
+    Serial.println("Failed to initialize IMU!");  //Shows if IMU module failed to activate
 
-    while (1);
+    while (1)
+      ;
   }
-  if(!BLE.begin()){
-    Serial.println("Failed to initialize BLE"); //Shows if BLE module failed to activate
-    while(1);  
+  if (!BLE.begin()) {
+    Serial.println("Failed to initialize BLE");  //Shows if BLE module failed to activate
+    while (1)
+      ;
   }
   BLE.setLocalName("IMUWearable");
-  BLE.setAdvertisedService(IMUService); // add the service UUID
-  IMUService.addCharacteristic(IMUSensorData); // add the IMU characteristic
-  BLE.addService(IMUService); // Add the IMU service
-  IMUSensorData.writeValue("Init"); // set initial value for this characteristic
-  
-  BLE.advertise(); // Display Service
+  BLE.setAdvertisedService(IMUService);         // add the service UUID
+  IMUService.addCharacteristic(IMUSensorData);  // add the IMU characteristic
+  BLE.addService(IMUService);                   // Add the IMU service
+  IMUSensorData.writeValue("Init");             // set initial value for this characteristic
+
+  BLE.advertise();  // Display Service
 
   String address = BLE.address();
   Serial.print("Waiting for connections, address is: ");
@@ -34,32 +37,34 @@ void setup() {
 }
 
 void loop() {
-  float x, y, z;
-  String package; // Create/clear a string to send data to central
-  BLEDevice central = BLE.central(); // Connect to central
-
-  if (IMU.accelerationAvailable()) { // If statement that sends code whenever there is acceleration acting on the IMU
-    IMU.readAcceleration(x, y, z); // Put the readings into floats for x y z vectors
-
-    // Build the string to send to central
-    package += String("C "); // Have start letter for central to process as IMU data just in case other data needs to be sent
-    package += String(x);
-    package += String(" ");
-    package += String(y);
-    package += String(" ");
-    package += String(z);
-    package += String(" ");
+  float x1, y1, z1, x2, y2, z2;
+  // Create/clear a string to send data to central
+  BLEDevice central = BLE.central();  // Wait for connection to central
+  if (central) {
+    Serial.println(central.address());
+    while (central.connected()) {
+      String package;
+      if (IMU.accelerationAvailable()) {  // If statement that sends code whenever there is acceleration acting on the IMU
+        IMU.readAcceleration(x1, y1, z1);    // Put the readings into floats for x y z vectors
+        IMU.readGyroscope(x2, y2, z2);    // Put the readings into floats for x y z tilt
+        // Build the string to send to central
+        package += String("C ");  // Have start letter for central to process as IMU data just in case other data needs to be sent
+        package += String(x1,2);
+        package += String(" ");
+        package += String(y1,2);
+        package += String(" ");
+        package += String(z1,2);
+        package += String(" ");
+        // Keep adding to the string
+        package += String(x2,2);
+        package += String(" ");
+        package += String(y2,2);
+        package += String(" ");
+        package += String(z2,2);
+        package += String(" ");
+        IMUSensorData.writeValue(package);  // Send data to central
+        Serial.println(package);
+      }
+    }
   }
-  if (IMU.gyroscopeAvailable()) {
-    IMU.readGyroscope(x, y, z); // Put the readings into floats for x y z tilt
-
-    // Keep adding to the string
-    package += String(x);
-    package += String(" ");
-    package += String(y);
-    package += String(" ");
-    package += String(z);
-  }
-
-  IMUSensorData.writeValue(package); // Send data to central
 }
