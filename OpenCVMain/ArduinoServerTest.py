486a50dc045e1b8a07c97cbfbd1d8f4706d126f7
K7eR0 <rcn045@gmail.com>
2025-07-16 10:51:07 -0400

    Threading
    Got threading to work for nano as a whole

--- OpenCVMain/ArduinoServerTest.py
+++ OpenCVMain/ArduinoServerTest.py
@@ -1,12 +1,15 @@
 import serial
 import socket
 import time
+import threading
 
 arduino = serial.Serial(port='COM12',   baudrate=115200, timeout=0.2)
 nano = serial.Serial(port='COM3',   baudrate=115200, timeout=0.2)
 
-while True:
-    package = nano.readline()[:-2].decode() # Decode the message from the nano and remove the new line command
-    arduino.write(package.encode()) # Re-encode the message and send to arduino
-    print(package) # Print the message from the nano
-    
+def readnano():
+    while True:
+        package = nano.readline()[:-2].decode() # Decode the message from the nano and remove the new line command
+        arduino.write(package.encode()) # Re-encode the message and send to arduino
+        print(package) # Print the message from the nano
+t = threading.Thread(target=readnano)
+t.start()    
